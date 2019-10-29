42535100e5b9b07c88ed6dfd2537c7b6f37971ae
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-10-29 14:26:30 -0400

    added a bunch of crafting recipes and json code to go with it.

--- src/main/java/trinsdar/pyrotech_compat/JsonMaker.java
+++ src/main/java/trinsdar/pyrotech_compat/JsonMaker.java
@@ -5,6 +5,9 @@ import com.google.gson.JsonObject;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonWriter;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import trinsdar.pyrotech_compat.block.rocks.BlockRockIgneous;
+import trinsdar.pyrotech_compat.block.rocks.BlockRockMetamorphic;
+import trinsdar.pyrotech_compat.block.rocks.BlockRockSedimentary;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -15,27 +18,129 @@ public class JsonMaker {
     private static String[] types = {"", "_Sandstone", "_sand"};
     public static void init(FMLPreInitializationEvent event){
         try {
-            JsonObject object = new JsonObject();
-            File file = new File("/home/trinsdar/IdeaProjects/modprojects/Pyrotech-Compat/src/main/resources/assets/pyrotech_compat/lang/en_us.json");
-            file.createNewFile();
-            JsonWriter writer = new JsonWriter(new FileWriter(file));
-            writer.beginObject();
-            writer.setIndent("  ");
-            for (String type : types){
-                for (String variant : variants){
-                    if (type.equalsIgnoreCase("_sand")){
-                        writer.name("tile.pyrotech_compat.rock_" + variant.toLowerCase() + type.toLowerCase() + ".name").value(variant.replace('_',' ')  + " Sand Pile");
-                    } else {
-                        writer.name("tile.pyrotech_compat.rock_" + variant.toLowerCase() + type.toLowerCase() + ".name").value(variant.replace('_',' ') + type.replace('_', ' ') + " Rock");
-                    }
+            for(int i = 0; i < 8; i ++){
+                JsonObject object = new JsonObject();
+                File file = new File("/home/trinsdar/IdeaProjects/modprojects/Pyrotech-Compat/src/main/resources/assets/pyrotech_compat/recipes/cobble" + BlockRockIgneous.EnumType.fromMeta(i).getName().replace("rock", "") + ".json");
+                file.createNewFile();
+                JsonWriter writer = new JsonWriter(new FileWriter(file));
+                writer.beginObject();
+                writer.setIndent("  ");
+                writer.name("type").value("minecraft:crafting_shaped");
+                writer.name("pattern");
+                writer.beginArray();
+                    writer.value("RRR");
+                    writer.value("RCR");
+                    writer.value("RRR");
+                writer.endArray();
+                writer.name("key");
+                writer.beginObject();
+                    writer.name("C");
+                    writer.beginObject();
+                        writer.name("item").value("pyrotech:material");
+                        writer.name("data").value(17);
+                    writer.endObject();
+                    writer.name("R");
+                    writer.beginObject();
+                        writer.name("item").value(PyrotechCompat.MODID + ":rock_igneous");
+                        writer.name("data").value(i);
+                    writer.endObject();
+                writer.endObject();
+                writer.name("result");
+                writer.beginObject();
+                    writer.name("item").value("undergroundbiome:igneous_cobblestone");
+                    writer.name("count").value(1);
+                    writer.name("data").value(i);
+                writer.endObject();
+                writer.endObject();
+                writer.close();
 
+                file = new File("/home/trinsdar/IdeaProjects/modprojects/Pyrotech-Compat/src/main/resources/assets/pyrotech_compat/recipes/cobble" + BlockRockMetamorphic.EnumType.fromMeta(i).getName().replace("rock", "") + ".json");
+                file.createNewFile();
+                writer = new JsonWriter(new FileWriter(file));
+                writer.beginObject();
+                writer.setIndent("  ");
+                writer.name("type").value("minecraft:crafting_shaped");
+                writer.name("pattern");
+                writer.beginArray();
+                    writer.value("RRR");
+                    writer.value("RCR");
+                    writer.value("RRR");
+                writer.endArray();
+                writer.name("key");
+                writer.beginObject();
+                    writer.name("C");
+                    writer.beginObject();
+                        writer.name("item").value("pyrotech:material");
+                        writer.name("data").value(17);
+                    writer.endObject();
+                    writer.name("R");
+                    writer.beginObject();
+                        writer.name("item").value(PyrotechCompat.MODID + ":rock_metamorphic");
+                        writer.name("data").value(i);
+                    writer.endObject();
+                writer.endObject();
+                writer.name("result");
+                writer.beginObject();
+                    writer.name("item").value("undergroundbiome:metamorphic_cobblestone");
+                    writer.name("count").value(1);
+                    writer.name("data").value(i);
+                writer.endObject();
+                writer.endObject();
+                writer.close();
+
+                if (i == 0 || i == 4){
+                    continue;
                 }
+
+                int meta = 0;
+                if (i < 4){
+                    meta = i - 1;
+                } else {
+                    meta = i - 2;
+                }
+
+                file = new File("/home/trinsdar/IdeaProjects/modprojects/Pyrotech-Compat/src/main/resources/assets/pyrotech_compat/recipes/stone" + getSedimentary(meta) + ".json");
+                file.createNewFile();
+                writer = new JsonWriter(new FileWriter(file));
+                writer.beginObject();
+                writer.setIndent("  ");
+                writer.name("type").value("minecraft:crafting_shaped");
+                writer.name("pattern");
+                writer.beginArray();
+                    writer.value("RRR");
+                    writer.value("RCR");
+                    writer.value("RRR");
+                writer.endArray();
+                writer.name("key");
+                writer.beginObject();
+                    writer.name("C");
+                    writer.beginObject();
+                        writer.name("item").value("pyrotech:material");
+                        writer.name("data").value(17);
+                    writer.endObject();
+                    writer.name("R");
+                    writer.beginObject();
+                        writer.name("item").value(PyrotechCompat.MODID + ":rock_sedimentary");
+                        writer.name("data").value(meta);
+                    writer.endObject();
+                writer.endObject();
+                writer.name("result");
+                writer.beginObject();
+                    writer.name("item").value("undergroundbiome:sedimentary_stone");
+                    writer.name("count").value(1);
+                    writer.name("data").value(i);
+                writer.endObject();
+                writer.endObject();
+                writer.close();
             }
-            writer.endObject();
-            writer.close();
+
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static String getSedimentary(int meta){
+        return BlockRockSedimentary.EnumType.fromMeta(meta).getName().replace("rock", "");
     }
 
     public static void writeTextureObject(JsonWriter writer, String variant, String type) throws IOException {
